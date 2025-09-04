963e0534bd16950fff39526a6dcc5dab51543de0
Vasheti <patrikskattberg@hotmail.com>
2025-09-04 10:59:32 +0200

    day 3 complete

--- day3/day3.py
+++ day3/day3.py
@@ -16,11 +16,21 @@ def read_file() -> str:
 def extract_mul(data: str) -> int:
     """go through the string and find the sequence mul(x,y)"""
     total = 0
-    matches = re.findall(r'(mul\(\d+,\d+\))', data)
-
+    matches = re.findall(r"(mul\(\d+,\d+\))|(don't\(\))|(do\(\))", data)
+    skip = False
     try:
         for match in matches:
-            number = re.findall(r'\d+', match)
+            if "don't()" in match:
+                skip = True
+
+            if "do()" in match:
+                skip = False
+                continue
+
+            if skip:
+                continue
+
+            number = re.findall(r'\d+', match[0])
             if number[0] and number[1]:
                 total += int(number[0]) * int(number[1])
                 
